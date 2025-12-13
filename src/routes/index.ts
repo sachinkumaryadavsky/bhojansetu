a7a07811625213700024c3a78d972455f31ce8bc
sachin kumar <sachinroysky@gmail.com>
2025-12-13 22:33:29 +0530

    feat:implemt register,login api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { FoodController } from "../controller/foodController";
+import { authController } from "../controller/authController";
+
 
 export default async function FoodRoutes(app: FastifyInstance) {
+   app.post("/register", authController.register);
+  app.post("/login", authController.login);
   app.post("/food", FoodController.create);
   app.get("/food", FoodController.getAll);
   app.post("/reservation",FoodController.reserveFood);
